f8cfbb16afff36ac9193f098264090e620040ff8
Ucheodiaka <ucheodiaka@gmail.com>
2023-06-04 21:04:29 +0100

    repearString Passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  if (times < 0) return 'ERROR'
+  let string = ''
+  for (let i = 0; i < times; i++) {
+    string += word
+  }
+  if (times < 10) {
+    return string
+  }
+  if (times === 1) {
+    return string
+  }
+  if (times === '') {
+    return string
+  }
+  if (times === -1) {
+    return 'ERROR'
+  }
+  if ('' === 10) {
+    return ''
+  }
 
-};
+  return string
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
